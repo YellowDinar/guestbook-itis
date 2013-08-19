b2f8a92879fc7cf21f316826ce4343f8b762d1ed
YellowDinar <kalimullindinrust@gmail.com>
2013-08-19 21:14:19 +0400

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Book::Application.routes.draw do
     resources:posts
     devise_for :users, :path => "auth", :path_names => { :sign_in => 'login', :sign_out => 'logout', :password => 'secret', :confirmation => 'verification', :unlock => 'unblock', :registration => 'register', :sign_up => 'cmon_let_me_in' }
     root 'posts#index'
-    get "devise/sessions#destroy"
+    delete "devise/sessions#destroy"
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
